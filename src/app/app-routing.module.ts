87efe0355c5768f00580f14055abe65547865f02
hasan-promact <hasan@promactinfo.com>
2018-05-15 17:24:58 +0530

    auto slide added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,11 @@ const routes: Routes = [
     path: '',
     component: HomeComponent,
     pathMatch: 'full'
+  },
+   {
+    path: 'home',
+    component: HomeComponent,
+    pathMatch: 'full'
   }
 ];
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,10 @@
-import { ChangeDetectionStrategy, Component, Input , OnInit } from '@angular/core';
+import { ChangeDetectionStrategy,ChangeDetectorRef, Component, Input , OnInit } from '@angular/core';
 import { trigger,style,transition,animate,keyframes,query,stagger,state } from '@angular/animations';
+import { Observable } from "rxjs";
+
 import { Image } from '../image';
 import { IMAGES } from '../images-list';
+
 
 type PaneType = 'left' | 'right';
 @Component({
@@ -17,6 +20,8 @@ type PaneType = 'left' | 'right';
         state('large', style({
             transform: 'scale(2)',
         })),
+        state('in', style({ 'opacity': '1' })),
+    state('out', style({ 'opacity': '0' })),
         transition('small <=> large', animate('.1s ease-in'))
     ]),
     trigger('images', [
@@ -32,10 +37,27 @@ type PaneType = 'left' | 'right';
           ]))]), {optional: true})
       ])
     ]),
+    trigger('fade', [
+    state('in', style({ 'opacity': '1' })),
+    state('out', style({ 'opacity': '0' })),
+    transition('* <=> *', [
+      animate(1000)
+    ])
+  ]),
      trigger('slide', [
       state('left', style({ transform: 'translateX(100%)' })),
       state('right', style({ transform: 'translateX(0%)' })),
       transition('* => *', animate(300))
+    ]),
+   trigger('show', [
+        transition(':enter', [
+            style({ opacity: 0 }),
+            animate(200, style({ opacity: 1 }))
+        ]),
+        transition(':leave', [
+            style({ opacity: 1 }),
+            animate(300, style({ opacity: 0 }))
+        ])
     ])
   ]
 })
@@ -44,33 +66,54 @@ images = IMAGES;
 zoomImage:Image;
 activePane: string = 'left';
 currIndex:number;
+autoSlide:number;
+totalImages:number = this.images.length;
+
 zoomMe(image, i) {
       this.zoomImage = image;
       this.activePane = 'right';
       this.currIndex = i;
+      this.autoSlide= setInterval( () => {
+        if(this.currIndex < this.images.length-1){
+        this.currIndex = this.currIndex +1;
+    }else{
+      this.currIndex = 0;
+    }
+    this.zoomImage = this.images[this.currIndex];
+    
+    this.ref.markForCheck();
+      },4000);
   }
   backslide(){
     this.activePane = "left";
+    clearInterval(this.autoSlide);
     return false;
   }
   zoomMeMore(image) {
+    clearInterval(this.autoSlide);
     image.state = (image.state === 'small' ? 'large' : 'small');
   }
   preImg(){
+    clearInterval(this.autoSlide);
     if(this.currIndex > 0){
+      
       this.currIndex = this.currIndex -1;
       this.zoomImage = this.images[this.currIndex];
+      
     }
     return false;
   }
   nxtImg(){
+    clearInterval(this.autoSlide);
     if(this.currIndex < this.images.length-1){
         this.currIndex = this.currIndex +1;
         this.zoomImage = this.images[this.currIndex];
     }
     return false;
   }
-  constructor() { }
+ constructor(private ref: ChangeDetectorRef) {
+
+ }
 
   ngOnInit() {
   }
