ffdcfb1e3fd2bbeda1291ab4a89bce0189f7334a
hasan-promact <hasan@promactinfo.com>
2018-05-15 14:31:08 +0530

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,62 @@
+import { ChangeDetectionStrategy, Component, Input , OnInit } from '@angular/core';
+import { trigger,style,transition,animate,keyframes,query,stagger,state } from '@angular/animations';
+import { Image } from '../image';
+import { IMAGES } from '../images-list';
+
+type PaneType = 'left' | 'right';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('imgZoom', [
+        state('small', style({
+            transform: 'scale(1)',
+        })),
+        state('large', style({
+            transform: 'scale(2)',
+        })),
+        transition('small <=> large', animate('.1s ease-in'))
+    ]),
+    trigger('images', [
+      transition('* => *', [
+
+        query(':enter', style({ opacity: 0 }), {optional: true}),
+
+        query(':enter', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
+          ]))]), {optional: true})
+      ])
+    ]),
+     trigger('slide', [
+      state('left', style({ transform: 'translateX(100%)' })),
+      state('right', style({ transform: 'translateX(0%)' })),
+      transition('* => *', animate(300))
+    ])
+  ]
+})
+export class HomeComponent implements OnInit {
+images = IMAGES;
+zoomImage:Image;
+activePane: string = 'left';
+zoomMe(image) {
+      this.zoomImage = image;
+      this.activePane = 'right';
+  }
+  backslide(){
+    this.activePane = "left";
+    return false;
+  }
+  zoomMeMore(image) {
+    image.state = (image.state === 'small' ? 'large' : 'small');
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/images-list.ts
@@ -0,0 +1,15 @@
+import { Image } from './image';
+export const IMAGES: Image[] = [
+  { id: 11, src: './assets/portfolio-1.jpg', state:"small" },
+  { id: 12, src: './assets/portfolio-2.jpg', state:"small" },
+  { id: 13, src: './assets/portfolio-3.jpg', state:"small" },
+  { id: 14, src: './assets/portfolio-4.jpg', state:"small" },
+  { id: 15, src: './assets/portfolio-5.jpg', state:"small" },
+  { id: 16, src: './assets/portfolio-6.jpg', state:"small" },
+  { id: 11, src: './assets/portfolio-1.jpg', state:"small" },
+  { id: 12, src: './assets/portfolio-2.jpg', state:"small" },
+  { id: 13, src: './assets/portfolio-3.jpg', state:"small" },
+  { id: 14, src: './assets/portfolio-4.jpg', state:"small" },
+  { id: 15, src: './assets/portfolio-5.jpg', state:"small" },
+  { id: 16, src: './assets/portfolio-6.jpg', state:"small" }
+];
